0274fb2bf428033962d3b2e2001ccec5b4a1a7f5
James McGlone <jamespmcglone@gmail.com>
2019-11-28 17:39:38 +0800

    Not ready for marking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,11 +16,15 @@
   
   let myKey;
   let gameKey;
+  let imHosting;
+  let playerInfo = {};
 
   let nickname;
   let nickcolor;
+  
 //   let pinger;
   let terminator;
+
 
 $('document').ready(function() {
     $('#nickname-text').select();
@@ -180,8 +184,9 @@ $('#rps-button').on("click", function(event) {
 function initiateGame(opponentKey, opponentNickname, opponentColor) {
     $('.game-card').css('display', 'none');
     $('.rps-card').css('display', 'block');
-    $('.rps-card').append(`Waiting for <font color="${opponentColor}">${opponentNickname}</font> to accept game!`)
+    $('#rps-content').append(`Waiting for <font color="${opponentColor}">${opponentNickname}</font> to accept game!`)
     
+
     database.ref('games').push({
         time: moment().format('HH:mm:ss'),
         hostColor: nickcolor,
@@ -189,46 +194,175 @@ function initiateGame(opponentKey, opponentNickname, opponentColor) {
         hostKey: myKey,
         game: `rps`,
         status: `invite`,
-        target: opponentKey
+        target: opponentKey,
+        targetNickname: opponentNickname,
+        targetColor: `red`,  //bit suss
+        hostMove: `none`,
+        targetMove: `none`
+
         });
 
 }
 
 database.ref('games').on("child_added", function(childSnapshot) {
-    
+    playerInfo = childSnapshot.val();
     // if type is a message, display it in the chat window
     if (childSnapshot.val().target == myKey && childSnapshot.val().status == `invite`) {
-
+        imHosting = false;
+        gameKey = childSnapshot.ref.key;
         $('.rps-card').css('display', 'block');
-        $('.rps-card').append(
+        $('#rps-content').append(
             `<font color="${childSnapshot.val().hostColor}">${childSnapshot.val().hostNickname}</font> has challenged you to a game of Rock Paper Scissors!<br>
-            <button onClick="acceptGame(childSnapshot)">Accept</button><button onClick="declineGame(childSnapshot)>Decline</button>
+            <button onClick="acceptGame()">Accept</button><button onClick="declineGame()">Decline</button>
             `);
+    } else if (childSnapshot.val().hostKey == myKey) {
+        gameKey = childSnapshot.ref.key;
+        imHosting = true;
     }
 
-    
+RPS();
+
 
 });
 
 
-function acceptGame(childSnapshot) {
-    gameKey = childSnapshot.ref.key;
-    
+function acceptGame() {
+
     database.ref('games/' + gameKey).update({
         time: moment().format('HH:mm:ss'),
         status: `connected`
      })  
 }
 
-function declineGame(childSnapshot) {
+function declineGame() {
+     database.ref('games/' + gameKey).remove();
+}
 
-    $('.rps-card').html(``);
-    $('.rps-card').css('display', 'none');
-    database.ref('games/' + gameKey).update({
-        time: moment().format('HH:mm:ss'),
-        status: `declined`
-     })  
+
+database.ref('games').on("child_removed", function(childSnapshot) {
+    
+   console.log(childSnapshot.ref.key);
+   console.log(gameKey);
+
+    if (childSnapshot.ref.key == gameKey) {
+        $('#rps-content').html(``);
+        $('.rps-card').css('display', 'none');
+    }
+    
+});
+
+let targetMove = `none`;
+let hostMove = `none`;
+let hostScore = 0;
+let targetScore = 0;
+
+function RPS() {
+    database.ref('games/' + gameKey).on("child_changed", function(snapshot) {
+        if (snapshot.val() == "connected") {
+            $('#rps-content').html(``);
+            $('#rps-content').html(`
+            <button onClick="myPick('rock')">Rock</button>
+            <button onClick="myPick('paper')">Paper</button>
+            <button onClick="myPick('scissors')">Scissors</button>
+            `);
+            
+        }
+        console.log(snapshot.val());
+
+
+
+        if (snapshot.ref.key == `targetMove`) {
+            targetMove = snapshot.val();
+            console.log('Set target');
+        }
+    
+        if (snapshot.ref.key == `hostMove`) {
+            hostMove = snapshot.val();
+            console.log('Set host');
+        }
+
+        if (hostMove != `none` && targetMove != `none`) {
+
+                let winner;
+                let outcomeDisplay;
+                if (hostMove == "rock") {
+                            if (targetMove == "rock") {
+                              winner = 'draw';
+                            } else if (targetMove == "paper") {
+                              winner = 'target';
+                            } else if (targetMove == "scissors") {
+                              winner = 'host';
+                            }
+                  }
+    
+    
+                    if (hostMove == "paper") {
+                            if (targetMove == "rock") {
+                              winner = 'host';
+                            } else if (targetMove == "paper") {
+                              winner = 'draw';
+                            } else if (targetMove == "scissors") {
+                              winner = 'target';
+                            }
+                    }
+    
+                      if (hostMove == "scissors") {
+                            if (targetMove == "rock") {
+                              winner = 'target';
+                            } else if (targetMove == "paper") {
+                              winner = 'host';
+                            } else if (targetMove == "scissors") {
+                              winner = 'draw';
+                            }
+                      }
+
+                      if (winner == 'draw') {
+                        outcomeDisplay = 'It is a draw';
+                      } else if (winner == 'host') {
+                        outcomeDisplay = playerInfo.hostNickname + ' wins';
+                        hostScore++;
+                      } else if (winner == 'target') {
+                        outcomeDisplay = playerInfo.targetNickname + ' wins';
+                        targetScore++;
+                      }
+
+            
+                      $('#rps-content').html(``);
+
+                      $('#rps-content').html(`
+                          ${playerInfo.hostNickname} chose: ${hostMove}<br>
+                          ${playerInfo.targetNickname} chose: ${targetMove}<br><br>
+                            ${outcomeDisplay}<br><br>
+                            Scoreboard:<br>
+                            ${playerInfo.hostNickname}: ${hostScore}
+                            ${playerInfo.targetNickname}: ${targetScore}
+                      `);
+
+
+
+
+
+
+
+        }
+        console.log(snapshot.val());
+    });
 
 
 }
 
+function myPick(move) {
+    if (imHosting) {
+        database.ref('games/' + gameKey).update({
+            
+            hostMove: move
+         })  
+    }
+
+    if (!imHosting) {
+        database.ref('games/' + gameKey).update({
+            
+            targetMove: move
+         })  
+    }
+}
